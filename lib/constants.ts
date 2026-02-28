b5c203b7d6a0097f6f10825dc8d8dce3b990d960
Israel-Beni <bendegbe72@gmail.com>
2026-02-28 23:31:49 +0100

    feat: Introduce new About, Contact, and Pricing pages, add multiple content sections, update global styles, and refine assets.

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,3 +5,12 @@ export const NAV_LINKS = [
   { name: 'Pricing', href: '/pricing' },
   { name: 'Contact', href: '/contact' }
 ] as const;
+
+export const CLIENTS = [
+  { name: 'Meta', id: "logo-light--meta_cjdys3" },
+  { name: 'US FOODS', id: "logo-light--bny-mellon_bnc7mp" },
+  { name: 'ADM', id: "logo-light--adm_elarlo" },
+  { name: 'Bank of America', id: "logo-light--band-of-america_d7pgrw" },
+  { name: 'BNY MELLON', id: "logo-light--band-of-america_d7pgrw" },
+  { name: "Kellogs", id: "logo-logo--kellogs_mnmem9" }
+];
